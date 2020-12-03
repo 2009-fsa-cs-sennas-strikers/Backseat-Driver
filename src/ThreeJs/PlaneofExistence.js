f440ca118dc9bb5547e68615aa64158e33da8afa
Winson <chanw835@gmail.com>
2020-12-03 17:25:59 -0500

    attempting to implement car velocity and collision

--- src/ThreeJs/PlaneofExistence.js
+++ src/ThreeJs/PlaneofExistence.js
@@ -3,16 +3,21 @@ import * as THREE from 'three'
 import { usePlane } from 'use-cannon'
 
 const Plane = (props) => {
-    const [ref] = usePlane(() => ({
+    const [planeRef] = usePlane(() => ({
       rotation: [-Math.PI /2,0,0],
+      mass: 0,
+      position: [0, 0, 0],
+      ...props
     }))
+    // position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0]
+    console.log('plane position', planeRef)
     return (
-      <mesh position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <planeBufferGeometry attach="geometry" args={[100, 100, 100]} />
+      <mesh ref={planeRef}>
+        <planeBufferGeometry attach="geometry" args={[100, 100]} />
         <meshStandardMaterial attach="material" color="black" />
       </mesh>
     )
-  
+
   }
 
 export default Plane
