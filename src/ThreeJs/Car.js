75b0754c1d1dbb43868764bec52212cfc6816c26
Mike Kim <mikekim.info@gmail.com>
2020-12-03 11:27:25 -0500

    fix: car with velocity

--- src/ThreeJs/Car.js
+++ src/ThreeJs/Car.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useBox } from 'use-cannon'
 import car from '../models/models/McLaren.glb'
 import lerp from 'lerp'
-import {PerspectiveCamera, OrbitControls} from 'drei'
+import {PerspectiveCamera, OrbitControls, draco} from 'drei'
 
 function keyboard(value) {
   let key = {};
@@ -56,43 +56,40 @@ const Car = (props) => {
   // position from state
   const [position, setPosition] = useState({x:0, y:1, z:0})
   const {x,y,z} = position
-    const [carRef, api] = useBox(() => ({mass:1}))
+  //carRef = three car in scene
+  //api = car's physics object
+    const [carRef, api] = useBox(() => ({mass:0.5}))
     // const carRef = useRef()
     const gltf = useLoader(GLTFLoader, car)
+
     const vec = new THREE.Vector3(x,y,z)
+
+    console.log('car model', gltf)
+    console.log('carref', carRef)
+    console.log('api', api)
   useFrame(() => {
     if (props.action === 'right' && x < 500) {
-      setPosition({
-        x: x+2,
-        z: z,
-      });
+      api.velocity.set(15,0,0);
       api.rotation.set(0, (Math.PI * 135/180), 0)
     }
     if (props.action === 'left' && x > -500) {
-      setPosition({
-        x: x-2,
-        z: z,
-      });
+      api.velocity.set(-15,0,0);
       api.rotation.set(0, (Math.PI * -45/180), 0)
     }
     if (props.action === 'up' && z > -500) {
-      setPosition({
-        x: x,
-        z: z-2,
-      });
+      api.velocity.set(0,0,-15);
       api.rotation.set(0, (Math.PI * 225/180), 0)
     }
     if (props.action === 'down' && z < 500) {
-      setPosition({
-        x: x,
-        z: z+2,
-      });
+      api.velocity.set(0,0,15);
       api.rotation.set(0, (Math.PI * 45/180), 0)
     }
     carRef.current.position.lerp(vec, 0.1)
+
     // carRef.current.rotation.x = lerp(carRef.current.rotation.x, (Math.PI / 2), 0.1)
   });
     return (
+      
       <>
     <PerspectiveCamera position={[position.x/10,0.1,position.z/10]} makeDefault={true} />
     <primitive ref={carRef} object={gltf.scene} scale={[10,10,10]} position={[0, 1, 0]} rotation={ [ 0, (Math.PI * -45/180), 0 ]} />
