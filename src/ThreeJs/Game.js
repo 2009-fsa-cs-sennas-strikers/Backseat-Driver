f60f86d5fc20b50beb6c7c87f72d128d00092809
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-12-07 08:57:20 -0800

    Update Game.js
    Winsons Block Changes

--- src/ThreeJs/Game.js
+++ src/ThreeJs/Game.js
@@ -34,30 +34,43 @@ class Game extends React.Component{
 
 render() {
 return (
-<Canvas>
-<Stars />
-<ambientLight intensity={0.5} />
-<spotLight castShadow intensity={1.5} position={[300, 300, 400]} />
-<Physics >
-  <Plane rotation={[-(Math.PI/2), 0, 0]} />
-  <Suspense fallback={<Box />}>{<Car action={this.state.action}/>}</Suspense>
-  <Block position={[-25,2.5,0]}/>
-  <Block position={[-20,2.5,0]}/>
-  <Block position={[-15,2.5,0]}/>
-  <Block position={[-10,2.5,0]}/>
-  <Block position={[-5,2.5,0]}/>
-  <BlockK position={[25,2.5,0]}/>
-  <BlockK position={[20,2.5,0]}/>
-  <BlockK position={[15,2.5,0]}/>
-  <BlockK position={[10,2.5,0]}/>
-  <BlockK position={[5,2.5,0]}/>
-  <SidePlane position={[0,0,-50]} />
-  <SidePlane rotation={[0,Math.PI,0]} position={[0,0, 50]} />
-  <SidePlane rotation={[0,-Math.PI/2,0]} position={[50,0,0]} />
-  <SidePlane rotation={[0,Math.PI/2,0]} position={[-50,0,0]} />
-</Physics >
-{/*<Viewport />*/}
-</Canvas>
+   <Canvas>
+        {/* <OrbitControls /> */}
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <spotLight castShadow intensity={1.5} position={[300, 300, 400]} />
+        <Physics gravity={[0, -100, 0]}>
+          <Plane rotation={[-(Math.PI/2), 0, 0]} />
+          <Suspense fallback={<Box />}>{<Car action={this.state.action}/>}</Suspense>
+          {/* One Row */}
+          <Block position={[-125,5,0]}/>
+          <Block position={[-75,5,0]}/>
+          <Block position={[-25,5,0]}/>
+          <Block position={[25,5,0]}/>
+          <Block position={[75,5,0]}/>
+          <Block position={[125,5,0]}/>
+          {/* One Row */}
+          <Block position={[-125,5,50]}/>
+          <Block position={[-75,5,50]}/>
+          <Block position={[-25,5,50]}/>
+          <Block position={[25,5,50]}/>
+          <Block position={[75,5,50]}/>
+          <Block position={[125,5,50]}/>
+          {/* One Row */}
+          <Block position={[-125,5,-50]}/>
+          <Block position={[-75,5,-50]}/>
+          <Block position={[-25,5,-50]}/>
+          <Block position={[25,5,-50]}/>
+          <Block position={[75,5,-50]}/>
+          <Block position={[125,5,-50]}/>
+          {/* One Row */}
+          <SidePlane position={[0,0,-150]} />
+          <SidePlane rotation={[0,Math.PI,0]} position={[0,0, 150]} />
+          <SidePlane rotation={[0,-Math.PI/2,0]} position={[150,0,0]} />
+          <SidePlane rotation={[0,Math.PI/2,0]} position={[-150,0,0]} />
+        </Physics>
+        <Viewport />
+      </Canvas>
     )
 }
 }
