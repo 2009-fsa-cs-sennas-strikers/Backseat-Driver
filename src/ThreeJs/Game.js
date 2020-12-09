02813f2241aa60ef5d641cc5450ed39dbc2d93b8
Winson <chanw835@gmail.com>
2020-12-09 11:57:04 -0500

    refactored tenserflow model and imported custom model

--- src/ThreeJs/Game.js
+++ src/ThreeJs/Game.js
@@ -1,6 +1,7 @@
 import React, { Suspense, } from 'react'
 import { Canvas } from 'react-three-fiber'
-import { loadModel, startListening } from '../tenserFlow'
+// import { loadModel, startListening } from '../tenserFlow'
+import Voice from '../tenserFlow/test'
 import { Stars, OrbitControls} from 'drei'
 import {Physics} from 'use-cannon'
 import Car from './Car'
@@ -13,6 +14,8 @@ import SidePlane from './SidePlane'
 import EndZone from './EndZone'
 import Stopwatch from './Stopwatch'
 
+const voice = new Voice()
+
 class Game extends React.Component{
     constructor(props) {
         super(props)
@@ -24,16 +27,18 @@ class Game extends React.Component{
         this.updatePosition = this.updatePosition.bind(this);
       }
 
-      componentDidMount() {
-        loadModel()
-        .then(() => startListening(this.voiceAction))
+      async componentDidMount() {
+        // loadModel()
+        // .then(() => startListening(this.voiceAction))
+        await voice.startListening(this.voiceAction)
       }
 
       voiceAction(command) {
         this.setState({
           action: command,
         });
-        console.log('voice command:', this.state.action);
+        console.log('command', command)
+        // console.log('voice command:', this.state.action);
       }
 
       // function to pass EndZone Position
@@ -45,73 +50,73 @@ class Game extends React.Component{
       }
 
 render() {
-return (
-  <>
-   <Stopwatch />
-   <Canvas>
-        {/* <OrbitControls /> */}
-        <Stars />
-        <ambientLight intensity={0.5} />
-        <spotLight castShadow intensity={1.5} position={[300, 300, 400]} />
-        <Physics gravity={[0, -100, 0]}>
-          <Plane rotation={[-(Math.PI/2), 0, 0]} />
-          <Suspense fallback={<Box />}>
-          {<Car action={this.state.action} position={[0,0,100]} />}
-          </Suspense>
-          <EndZone />
-          {/* One Row */}
-          <Block position={[-125,5,100]}/>
-          <Block position={[-75,5,100]}/>
-          <Block position={[-25,5,100]}/>
-          <Block position={[25,5,100]}/>
-          <Block position={[75,5,100]}/>
-          <Block position={[125,5,100]}/>
-          {/* One Row */}
-          <Block position={[-125,5,50]}/>
-          <Block position={[-75,5,50]}/>
-          <Block position={[-25,5,50]}/>
-          <Block position={[25,5,50]}/>
-          <Block position={[75,5,50]}/>
-          <Block position={[125,5,50]}/>
-          {/* One Row */}
-          <Block position={[-125,5,0]}/>
-          <Block position={[-75,5,0]}/>
-          <Block position={[-25,5,0]}/>
-          <Block position={[25,5,0]}/>
-          <Block position={[75,5,0]}/>
-          <Block position={[125,5,0]}/>
-          {/* One Row */}
-          <Block position={[-125,5,50]}/>
-          <Block position={[-75,5,50]}/>
-          <Block position={[-25,5,50]}/>
-          <Block position={[25,5,50]}/>
-          <Block position={[75,5,50]}/>
-          <Block position={[125,5,50]}/>
-          {/* One Row */}
-          <Block position={[-125,5,-50]}/>
-          <Block position={[-75,5,-50]}/>
-          <Block position={[-25,5,-50]}/>
-          <Block position={[25,5,-50]}/>
-          <Block position={[75,5,-50]}/>
-          <Block position={[125,5,-50]}/>
-          {/* One Row */}
-          <Block position={[-125,5,-100]}/>
-          <Block position={[-75,5,-100]}/>
-          <Block position={[-25,5,-100]}/>
-          <Block position={[25,5,-100]}/>
-          <Block position={[75,5,-100]}/>
-          <Block position={[125,5,-100]}/>
-          {/* One Row */}
-          <SidePlane position={[0,0,-150]} />
-          <SidePlane rotation={[0,Math.PI,0]} position={[0,0, 150]} />
-          <SidePlane rotation={[0,-Math.PI/2,0]} position={[150,0,0]} />
-          <SidePlane rotation={[0,Math.PI/2,0]} position={[-150,0,0]} />
-        </Physics>
-        {/* <Viewport /> */}
-      </Canvas>
-      </>
-    )
-}
+  return (
+    <>
+      <Stopwatch />
+      <Canvas>
+          {/* <OrbitControls /> */}
+          <Stars />
+          <ambientLight intensity={0.5} />
+          <spotLight castShadow intensity={1.5} position={[300, 300, 400]} />
+          <Physics gravity={[0, -100, 0]}>
+            <Plane rotation={[-(Math.PI/2), 0, 0]} />
+            <Suspense fallback={<Box />}>
+            {<Car action={this.state.action} position={[0,0,100]} />}
+            </Suspense>
+            <EndZone />
+            {/* One Row */}
+            <Block position={[-125,5,100]}/>
+            <Block position={[-75,5,100]}/>
+            <Block position={[-25,5,100]}/>
+            <Block position={[25,5,100]}/>
+            <Block position={[75,5,100]}/>
+            <Block position={[125,5,100]}/>
+            {/* One Row */}
+            <Block position={[-125,5,50]}/>
+            <Block position={[-75,5,50]}/>
+            <Block position={[-25,5,50]}/>
+            <Block position={[25,5,50]}/>
+            <Block position={[75,5,50]}/>
+            <Block position={[125,5,50]}/>
+            {/* One Row */}
+            <Block position={[-125,5,0]}/>
+            <Block position={[-75,5,0]}/>
+            <Block position={[-25,5,0]}/>
+            <Block position={[25,5,0]}/>
+            <Block position={[75,5,0]}/>
+            <Block position={[125,5,0]}/>
+            {/* One Row */}
+            <Block position={[-125,5,50]}/>
+            <Block position={[-75,5,50]}/>
+            <Block position={[-25,5,50]}/>
+            <Block position={[25,5,50]}/>
+            <Block position={[75,5,50]}/>
+            <Block position={[125,5,50]}/>
+            {/* One Row */}
+            <Block position={[-125,5,-50]}/>
+            <Block position={[-75,5,-50]}/>
+            <Block position={[-25,5,-50]}/>
+            <Block position={[25,5,-50]}/>
+            <Block position={[75,5,-50]}/>
+            <Block position={[125,5,-50]}/>
+            {/* One Row */}
+            <Block position={[-125,5,-100]}/>
+            <Block position={[-75,5,-100]}/>
+            <Block position={[-25,5,-100]}/>
+            <Block position={[25,5,-100]}/>
+            <Block position={[75,5,-100]}/>
+            <Block position={[125,5,-100]}/>
+            {/* One Row */}
+            <SidePlane position={[0,0,-150]} />
+            <SidePlane rotation={[0,Math.PI,0]} position={[0,0, 150]} />
+            <SidePlane rotation={[0,-Math.PI/2,0]} position={[150,0,0]} />
+            <SidePlane rotation={[0,Math.PI/2,0]} position={[-150,0,0]} />
+          </Physics>
+          {/* <Viewport /> */}
+        </Canvas>
+        </>
+      )
+  }
 }
 
 export default Game
