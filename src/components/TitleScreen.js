c1df4d47d6febad28bdca52cc50f5318092e0fed
Mike Kim <mikekim.info@gmail.com>
2020-12-10 13:30:20 -0500

    basic react-component styling

--- src/components/TitleScreen.js
+++ src/components/TitleScreen.js
@@ -6,11 +6,13 @@ class Title extends React.Component {
     constructor(props){
         super(props)
     }
-    
+
 
     render(){
         return (<div id="home">
-        <div id="title">BackSeat Driver</div>
+        <div id="title"><h1>Backseat Driver</h1><h2>A voice-command driven 3D interactive simulation</h2></div>
+        <div>Please backseat drive responsibly.</div>
+
         <button id="playButton" onClick={() => this.props.changePlaying()}>Testing</button>
         <div id="github">
             <a href="https://github.com/2009-fsa-cs-sennas-strikers/Backseat-Driver">
