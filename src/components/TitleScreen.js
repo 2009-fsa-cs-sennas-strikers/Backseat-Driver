c9bd7e12d625081cc1d5f9da4df6bec3c8b80590
Mike Kim <mikekim.info@gmail.com>
2020-12-10 15:27:24 -0500

    wip: additional react component styling

--- src/components/TitleScreen.js
+++ src/components/TitleScreen.js
@@ -11,9 +11,9 @@ class Title extends React.Component {
     render(){
         return (<div id="home">
         <div id="title"><h1>Backseat Driver</h1><h2>A voice-command driven 3D interactive simulation</h2></div>
-        <div>Please backseat drive responsibly.</div>
+        <div><p>Please backseat drive responsibly.</p></div>
 
-        <button id="playButton" onClick={() => this.props.changePlaying()}>Testing</button>
+        <button id="playButton" onClick={() => this.props.changePlaying()}>Drive</button>
         <div id="github">
             <a href="https://github.com/2009-fsa-cs-sennas-strikers/Backseat-Driver">
                 <img src="github.png"/>
